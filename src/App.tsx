55a195117c22c97ab5d4681f848a958d257823e7
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 10:37:21 +0900

    add hover and transition effect in each cell

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-3 p-4 min-h-dvh">
-      <div className="border flex items-center justify-center col-span-2">
+    <div className="grid grid-cols-2 grid-rows-2 gap-3 p-2 min-h-dvh">
+      <div className="border flex items-center justify-center col-span-2 hover:bg-red-200 cursor-pinter transition duration-300">
         こんにちは
       </div>
-      <div className="border flex items-center justify-center">こんにちは</div>
-      <div className="border flex items-center justify-center">こんにちは</div>
-      <div className="border flex items-center justify-center">こんにちは</div>
+      <div className="border flex items-center justify-center hover:bg-red-200 cursor-pinter transition duration-300">
+        こんにちは
+      </div>
+      <div className="border flex items-center justify-center hover:bg-red-200 cursor-pinter transition duration-300">
+        こんにちは
+      </div>
     </div>
   );
 }
