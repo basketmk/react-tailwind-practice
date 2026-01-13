dfcbb8d2d30a5ee3aca3bfc631721beead863606
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 11:16:15 +0900

    Tailwindの共通クラスの改善

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,21 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const colorChange = "hover:bg-red-200 cursor-pointer transition duration-300";
+  const baseBox = "border flex items-center justify-center cursor-pointer ";
+
+  const hoverRed = "hover:bg-red-200 transition-all duration-300";
 
   const [title, setTitle] = useState("TailWind 練習用", 0);
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-3 p-2 min-h-dvh">
       <div
-        className={`border flex items-center justify-center col-span-2 text-5xl  ${colorChange}`}
+        className={`${baseBox} ${hoverRed} col-span-2 text-5xl `}
         onClick={() => setTitle("クリックされました")}
       >
         {title}
       </div>
-      <div className={`border flex items-center justify-center ${colorChange}`}>
-        こんにちは
-      </div>
-      <div className={`border flex items-center justify-center ${colorChange}`}>
-        こんにちは
-      </div>
+      <div className={`${baseBox} ${hoverRed}`}>こんにちは</div>
+      <div className={`${baseBox} ${hoverRed}`}>こんにちは</div>
     </div>
   );
 }
