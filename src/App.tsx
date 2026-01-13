608f20750e891e754ca531ce81c89343b53c4a01
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 10:47:54 +0900

    refactor shared tailwind classes into variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const colorChange = "hover:bg-red-200 cursor-pinter transition duration-300";
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-3 p-2 min-h-dvh">
-      <div className="border flex items-center justify-center col-span-2 hover:bg-red-200 cursor-pinter transition duration-300">
+      <div
+        className={`border flex items-center justify-center col-span-2 ${colorChange}`}
+      >
         こんにちは
       </div>
-      <div className="border flex items-center justify-center hover:bg-red-200 cursor-pinter transition duration-300">
+      <div className={`border flex items-center justify-center ${colorChange}`}>
         こんにちは
       </div>
-      <div className="border flex items-center justify-center hover:bg-red-200 cursor-pinter transition duration-300">
+      <div className={`border flex items-center justify-center ${colorChange}`}>
         こんにちは
       </div>
     </div>
